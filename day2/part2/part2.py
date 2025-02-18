a9d538f487ec1e8a5875e632f510df975c73c3d5
ruben450 <nemesruben@gmail.com>
2025-02-19 00:17:18 +0200

    day2 done

--- day2/part2/part2.py
+++ day2/part2/part2.py
@@ -2,20 +2,52 @@ file = open("../data.txt", "r")
 content = file.readlines()
 file.close()
 
-total = 0
+totalSafe = 0
 
-leftNumbers = []
-rightNumbers = []
+def IsSafe(numbers):
+    i = 1
+    inc = 2
+    safe = 0
+    while i < len(numbers):
+        a = int(numbers[i - 1])
+        b = int(numbers[i])
+        if a > b:
+            if inc != 0:
+                inc = 1
+                safe = 1
+            else:
+                safe = 0
+        else:
+            if inc != 1:
+                inc = 0
+                safe = 1
+            else:
+                safe = 0
+        if safe == False:
+            return False
+        diff = abs(a - b)
+        if diff >= 1 and diff <= 3:
+            safe = 1
+        else:
+            safe = 0
+        if safe == 0:
+            return False
+        i+=1
+    return bool(safe)
 
 for line in content:
     numbers = line.split()
-    a = int(numbers[0])
-    b = int(numbers[1])
-    leftNumbers.append(a)
-    rightNumbers.append(b)
+    safe = 0
+    if IsSafe(numbers):
+        safe = 1
+    else:
+        for i in range(len(numbers)):
+            temp = numbers.copy()
+            del temp[i]
+            if IsSafe(temp):
+                safe = 1
+    totalSafe += safe
 
-for a in leftNumbers:
-    appears = rightNumbers.count(a)
-    total += a * appears 
-
-print(total)
+print(totalSafe)
+                
+        
