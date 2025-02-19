3d91de722191e52805d405ae76636418b82fee07
ruben450 <nemesruben@gmail.com>
2025-02-19 22:46:51 +0200

    solved part2 with youtube, my regex is not that good

--- day3/part2/part2.py
+++ day3/part2/part2.py
@@ -1,53 +1,20 @@
+import re
+
 file = open("../data.txt", "r")
 content = file.readlines()
 file.close()
 
-totalSafe = 0
-
-def IsSafe(numbers):
-    i = 1
-    inc = 2
-    safe = 0
-    while i < len(numbers):
-        a = int(numbers[i - 1])
-        b = int(numbers[i])
-        if a > b:
-            if inc != 0:
-                inc = 1
-                safe = 1
-            else:
-                safe = 0
-        else:
-            if inc != 1:
-                inc = 0
-                safe = 1
-            else:
-                safe = 0
-        if safe == False:
-            return False
-        diff = abs(a - b)
-        if diff >= 1 and diff <= 3:
-            safe = 1
-        else:
-            safe = 0
-        if safe == 0:
-            return False
-        i+=1
-    return bool(safe)
-
+total = 0
+enabled = True
 for line in content:
-    numbers = line.split()
-    safe = 0
-    if IsSafe(numbers):
-        safe = 1
-    else:
-        for i in range(len(numbers)):
-            temp = numbers.copy()
-            del temp[i]
-            if IsSafe(temp):
-                safe = 1
-    totalSafe += safe
+    matches = re.findall(r"(?:mul\((\d+),(\d+)\))|(do\(\)|don't\(\))",line)
+    for match in matches:
+        if match[2] == "" and enabled:
+            total += int(match[0]) * int(match[1])
+        else:
+            if match[2] == "do()":
+                enabled = True
+            else:
+                enabled = False
 
-print(totalSafe)
-                
-        
+print(total)
