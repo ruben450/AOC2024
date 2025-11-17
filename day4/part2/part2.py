0edfb436cd6604146ded41b166bc3476fa48cc28
ruben450 <nemesruben@gmail.com>
2025-11-18 01:30:06 +0200

    minor changes to solve part2 of day4

--- day4/part2/part2.py
+++ day4/part2/part2.py
@@ -5,14 +5,8 @@ file.close()
 total = 0
 grid = []
 directions = [
-    (0,  1),
     ( 1,  1),
-    ( 1, 0),
     ( 1,  -1),
-    (0, -1),
-    (-1,  -1),
-    ( -1, 0),
-    ( -1,  1)
 ]
 word = 'XMAS'
 
@@ -21,21 +15,24 @@ for line in content:
 
 for i in range(len(grid)):
     for j in range(len(grid[i])):
-        if grid[i][j] == 'X':
+        if grid[i][j] == 'A':
+            diagonal1 = False
             for x,y in directions:
                 newX = i + x
                 newY = j + y
+                prevLetter = ''
                 if 0 <= newX < len(grid) and 0 <= newY < len(grid[0]):
-                    if grid[newX][newY] == 'M':
-                        newX = newX + x
-                        newY = newY + y
+                    if grid[newX][newY] == 'M' or grid[newX][newY] == 'S':
+                        prevLetter = grid[newX][newY]
+                        newX = i - x
+                        newY = j - y
                         if 0 <= newX < len(grid) and 0 <= newY < len(grid[0]):
-                            if grid[newX][newY] == 'A':
-                                newX = newX + x
-                                newY = newY + y
-                                if 0 <= newX < len(grid) and 0 <= newY < len(grid[0]):
-                                    if grid[newX][newY] == 'S':
-                                        total += 1
+                            if grid[newX][newY] == 'M' or grid[newX][newY] == 'S':
+                                if prevLetter != grid[newX][newY]:
+                                    if not diagonal1: diagonal1 = True
+                                    else: total+=1
+
+                                    
 
 
 print(total)
