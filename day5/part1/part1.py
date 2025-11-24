eb3eac235999bc21de110cf52dbbe8fc768a14cd
ruben450 <nemesruben@gmail.com>
2025-11-24 18:49:06 +0200

    solved part1 of day5

--- day5/part1/part1.py
+++ day5/part1/part1.py
@@ -2,7 +2,40 @@ file = open("../data.txt", "r")
 content = file.readlines()
 file.close()
 
-total = 0
+rules = []
+updates = []
+locUpdate = 0
+for index, line in enumerate(content):
+    if line.strip() == "":
+        locUpdate = index + 1
+        break
+    rules.append(line.strip().split('|'))
 
+for line in content[locUpdate:]:
+    updates.append(line.strip())
+
+correctUpdates = []
+for update in updates:
+    updateBlocks = update.split(',')
+    badUpdate = False
+    for index, x in enumerate(updateBlocks):
+        for rule in rules:
+            if x == rule[0]:
+                if rule[1] in updateBlocks:
+                    if rule[1] not in updateBlocks[index+1:]:
+                        badUpdate = True
+                        break
+            if index > 0:
+                if x == rule[1]:
+                    if rule[0] in updateBlocks:
+                        if rule[0] not in updateBlocks[:index+1]:
+                            badUpdate = True
+                            break
+    if not badUpdate:
+            correctUpdates.append(update)
+
+total = 0
+for update in correctUpdates:
+    update = update.split(',')
+    total += int(update[len(update)//2])
 print(total)
-        
