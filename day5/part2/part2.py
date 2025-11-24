bf8c86655db8a9e9b366fc077785b1ccbf078a28
ruben450 <nemesruben@gmail.com>
2025-11-24 20:09:08 +0200

    solved part2 with help of chatgpt was stuck on the sawpping part

--- day5/part2/part2.py
+++ day5/part2/part2.py
@@ -2,6 +2,55 @@ file = open("../data.txt", "r")
 content = file.readlines()
 file.close()
 
-total = 0
+rules = []
+updates = []
+locUpdate = 0
+for index, line in enumerate(content):
+    if line.strip() == "":
+        locUpdate = index + 1
+        break
+    rules.append(line.strip().split('|'))
 
+for line in content[locUpdate:]:
+    updates.append(line.strip())
+
+correctUpdates = []
+for update in updates:
+    updateBlocks = update.split(',')
+    badUpdate = False
+    for index, x in enumerate(updateBlocks):
+        for rule in rules:
+            if x == rule[0]:
+                if rule[1] in updateBlocks:
+                    if rule[1] not in updateBlocks[index+1:]:
+                        badUpdate = True
+                        break
+            if index > 0:
+                if x == rule[1]:
+                    if rule[0] in updateBlocks:
+                        if rule[0] not in updateBlocks[:index+1]:
+                            badUpdate = True
+                            break
+    if badUpdate:
+        correctUpdates.append(update)
+
+orderedCorrectUpdates = []
+for update in correctUpdates:
+    correctOrder = update.split(',').copy()
+    swapped = True
+    while swapped:
+        swapped = False
+        for x in correctOrder:
+            for rule in rules:
+                if x == rule[0] and rule[1] in correctOrder:
+                    a = correctOrder.index(rule[0])
+                    b = correctOrder.index(rule[1])
+                    if a > b:
+                        correctOrder[a], correctOrder[b] = correctOrder[b], correctOrder[a]
+                        swapped = True
+    orderedCorrectUpdates.append(correctOrder)
+
+total = 0
+for update in orderedCorrectUpdates:
+    total += int(update[len(update)//2])
 print(total)
