1d33943bce574f290cf552d7bde85ddbe575003e
ruben450 <nemesruben@gmail.com>
2025-11-29 17:27:50 +0200

    solved day2 but code takes long to process

--- day7/part2/part2.py
+++ day7/part2/part2.py
@@ -2,7 +2,24 @@ file = open("../data.txt", "r")
 content = file.readlines()
 file.close()
 
-map = []
+from itertools import product
 
+total = 0
 for line in content:
-    map.append(list(line.strip()))
+    answer = int(line.strip().split(':')[0])
+    numbers = line.strip().split(':')[1].strip().split(' ')
+    numbers = [int(number) for number in numbers]
+    for operators in product(['||', '+', '*'], repeat = len(numbers) -1):
+        sum = numbers[0]
+        for i, op in enumerate(operators):
+            if op == '||':
+                temp = sum
+                sum = int(str(sum) + str(numbers[i+1]))
+            if op == '+':
+                sum += numbers[i+1]
+            if op == '*':
+                sum *= numbers[i+1]
+        if answer == sum:
+            total+=sum
+            break
+print(total)
